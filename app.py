52dcc3df83654e405e5d89e3ef75665a7939c67f
shrinksboy <jonahcasimir@gmail.com>
2025-02-11 18:58:39 -0600

    change app.run params

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ import os
 
 from flask import Flask, request, jsonify
 
-import logging
+# import logging
 
 app = Flask(__name__)
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
 
 # @app.route('/', methods=['POST'])
 # def webhook_handler():
@@ -100,5 +100,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    # Don't use this in production.  Gunicorn will handle the serving.
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
