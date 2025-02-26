3da21e3b6344e86d985fc80baaa1b20bd141e196
shrinksboy <jonahcasimir@gmail.com>
2025-02-25 20:06:32 -0600

    change safety settings

--- app.py
+++ app.py
@@ -262,7 +262,30 @@ def gemini_request(input_text, image=None):
                                     history=get_chat_history_from_firestore("dev-logs"),
                                     config=types.GenerateContentConfig(
                                         system_instruction=sys_instruct,
-                                        # safety_settings=safety_settings
+                                        safety_settings =[types.SafetySetting(
+                                                    category=types.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                                                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
+                                                ),
+                                                types.SafetySetting(
+                                                    category=types.HarmCategory.HARM_CATEGORY_HARASSMENT,
+                                                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
+                                                ),
+                                                types.SafetySetting(
+                                                    category=types.HarmCategory.HARM_CATEGORY_CIVIC_INTEGRITY,
+                                                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
+                                                ),
+                                                types.SafetySetting(
+                                                    category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                                                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
+                                                ),
+                                                types.SafetySetting(
+                                                    category=types.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                                                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
+                                                ),
+                                                types.SafetySetting(
+                                                    category=types.HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+                                                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
+                                                )]
                                         )
                                     )
          response = chat.send_message(input_text + "")
